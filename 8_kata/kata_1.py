f029324e9d0a8e643730cfec20da34ccc3f801ec
VitaliyLitvinov <v.litvinov2000@gmail.com>
2024-11-25 20:27:09 +0300

    first commit

---
+++ 8_kata/kata_1.py
@@ -0,0 +1,7 @@
+# Make a function that will return a greeting statement
+# that uses an input; your program should return,
+# "Hello, <name> how are you doing today?".
+def greet(name):
+    return "Hello, " + str(name) + " how are you doing today?"
+    #Good Luck (like you need it)
+    pass
