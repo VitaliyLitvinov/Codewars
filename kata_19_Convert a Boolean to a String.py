6e51b9a91579dc164145e5956039a36abed6bfd2
VitaliyLitvinov <v.litvinov2000@gmail.com>
2024-12-03 22:20:21 +0300

    commit

---
+++ kata_19_Convert a Boolean to a String.py
@@ -0,0 +1,8 @@
+# mplement a function which convert the given boolean
+# value into its string representation.
+
+def boolean_to_string(b):
+    if b == True:
+        return 'True'
+    else:
+        return 'False'
