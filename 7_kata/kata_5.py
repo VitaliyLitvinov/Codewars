65885e865dc7a39e14b6d00602c4d891244c5377
Vitaly_NB <v.litvinov2000@gmail.com>
2025-01-10 15:55:57 +0300

    commit

---
+++ 7_kata/kata_5.py
@@ -0,0 +1,8 @@
+def solution(number):
+    list_number = set()
+    for i in range(1, number):
+        if i < 0:
+            return 0
+        elif i % 3 == 0 or i % 5 == 0:
+            list_number.add(i)
+    return sum(list_number)
